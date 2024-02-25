bd0bd2f9002bd14b0dd3274684817652e73bac4a
Amanda Rocha Leite de Araújo <amndrocha@gmail.com>
2024-02-25 13:10:48 -0300

    add map

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Messages from './Messages'
 import Account from './Account'
 
 function App() {
-  const [current, setCurrent] = useState('account')
+  const [current, setCurrent] = useState('map');
 
   return (
     <div>
-        <div className='header'>
+        <div className='content'>
+          <span className={current == 'map' ? 'visible' : 'none'}><Map/></span>
+          <span className={current == 'account' ? 'visible' : 'none'}><Account/></span>
+          <span className={current == 'messages' ? 'visible' : 'none'}><Messages/></span>
+          <span className={current == '' ? 'visible' : 'none'}></span>          
+        </div>
+
+        <div className='menu'>
             <div className='navlink' onClick={() => setCurrent('map')}>Mapa</div>
             <div className='navlink' onClick={() => setCurrent('messages')}>Mensagens</div>
             <div className='navlink' onClick={() => setCurrent('account')}>Conta</div>
-        </div>
-        <span className={current == 'map' ? 'visible' : 'none'}><Map/></span>
-        <span className={current == 'account' ? 'visible' : 'none'}><Account/></span>
-        <span className={current == 'messages' ? 'visible' : 'none'}><Messages/></span>
-        <span className={current == '' ? 'visible' : 'none'}></span>        
+        </div>        
     </div>
   )
 }
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,7 +1,193 @@
+import { divIcon } from "leaflet";
+import { useState } from "react";
+import { MapContainer, Marker, TileLayer, useMapEvents } from "react-leaflet";
+import { renderToString } from 'react-dom/server';
+import './Map.css'
+
 function Map() {
+    const [openModal, setOpenModal] = useState(false);
+    const [dark, setDark] = useState(false);
+    const [pass, setPass] = useState('');
+    const [input, setInput] = useState('');
+    const [countries, setCountries] = useState([
+        {
+            name: 'brasil',
+            pass: 'brasil',
+            position: [ -2.7562580015438476 , -48.17132509212502 ],
+            marker: 'locked-point',
+        },
+        {
+            name: 'argentina',
+            pass: 'argentina',
+            position: [ -40.77563875754914 , -69.2750762182338 ],
+            marker: 'unlocked-point',
+        },
+        {
+            name: 'eua',
+            pass: 'eua',
+            position: [ 33.71432359567805 , -86.95083968378019 ],
+            marker: 'locked-point',
+        },
+        {
+            name: 'polonia',
+            pass: 'polonia',
+            position: [ 51.57581250779703 , 20.601412987576932 ],
+            marker: 'locked-point',
+        },
+        {
+            name: 'turquia',
+            pass: 'turquia',
+            position: [ 39.87468079676904 , 35.40007298068552 ],
+            marker: 'locked-point',
+        },
+        {
+            name: 'mocambique',
+            pass: 'mocambique',
+            position: [ -9.915867897227745 , 27.67686458128958 ],
+            marker: 'unlocked-point',
+        },
+        {
+            name: 'arabia',
+            pass: 'arabia',
+            position: [ 25.73921961430858 , 42.000545531346155 ],
+            marker: 'locked-point',
+        },
+        {
+            name: 'india',
+            pass: 'india',
+            position: [ 21.16301794100615 , 79.60419371933031 ],
+            marker: 'locked-point',
+        },
+        {
+            name: 'indonesia',
+            pass: 'indonesia',
+            position: [ -0.396793751120472 , 101.92963933255506 ],
+            marker: 'unlocked-point',
+        },
+        {
+            name: 'turcomenistao',
+            pass: 'turcomenistao',
+            position: [ 39.4008965162013 , 58.422772177048984 ],
+            marker: 'locked-point',
+        },  
+    ]);
+
+    const handleModal = (x) => {
+        if (openModal) {
+            setOpenModal(false);
+            setPass('');
+        } else {
+            setOpenModal(true);
+            setPass(x);
+        }
+    };
+    const checkPass = () => {
+        if (pass == input) {
+            countries.map((country) => {
+                if (country.pass == pass) {
+                    country.marker = dark ? "dark-unlocked-point" : "unlocked-point";
+                }
+            })            
+        }
+        setInput('');
+    };
+    const handleDark = () => {
+        setDark(!dark);
+        setCountries(countries.map((country) => {
+            if (country.marker == "locked-point") {
+                return {
+                    ...country,
+                    marker: "dark-locked-point"
+                };
+            }
+            if (country.marker == "unlocked-point") {
+                return {
+                    ...country,
+                    marker: "dark-unlocked-point"
+                };
+            }
+            if (country.marker == "dark-unlocked-point") {
+                return {
+                    ...country,
+                    marker: "unlocked-point"
+                };
+            }
+            if (country.marker == "dark-locked-point") {
+                return {
+                    ...country,
+                    marker: "locked-point"
+                };
+            }
+        }))
+    };
+
+    const LocationFinderDummy = () => {
+        const map = useMapEvents({
+            click(e) {
+                console.log('[', e.latlng.lat,',',e.latlng.lng,']');
+            },
+        });
+        return null;
+    };
+
     return (
-        <div>
-            Mapa
+        <div className={dark ? 'dark-map-page' : 'map-page'} >
+            <div className={ dark ? "lightBtnOff" : "lightBtnOn"} onClick={handleDark}><div className={ dark ? "lightBtnCircleOff" : "lightBtnCircleOn"}></div></div>
+            <div className={openModal ? 'modal' : 'none'}>
+                <div className="modal-window">
+                    <div className="modal-header">
+                        <div className="close-btn" onClick={handleModal}>X</div>
+                    </div>
+                    <div className="modal-content">
+                        {countries.map((country, i) => {
+                            if (country.pass == pass) {
+                                if (country.marker == "unlocked-point" || country.marker == "dark-unlocked-point") {
+                                    return(
+                                        <div key={country}>País desbloqueado!! [inserir charada do próximo país]</div>
+                                    )
+                                } else {
+                                    return(
+                                        <div className="input-box" key={i}>
+                                            <input id="pass" type="text" value={input}
+                                            onChange={(e) => setInput(e.target.value)}
+                                            onKeyDown={(e) => {if (e.key === "Enter") checkPass()}}
+                                            />
+                                            <button onClick={checkPass}>Enviar</button>
+                                        </div>
+                                    )
+                                }
+                            }
+                        })}                            
+                    </div>
+                </div>
+            </div>
+
+            <MapContainer center={[0,0]} zoom={2} minZoom={2}>
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                opacity={dark ? 0.1 : 1}
+                />
+                {countries.map((country) => {
+                    return(
+                        <Marker 
+                        position={country.position}
+                        icon={
+                            divIcon({
+                                html: renderToString(<div className={country.marker}></div>),
+                                className: "",
+                            })
+                        }
+                        eventHandlers={{
+                            click: () => handleModal(country.pass),
+                        }}
+                        key={country.name}
+                        >
+                        </Marker>
+                    )
+                })}
+                <LocationFinderDummy/>
+            </MapContainer>            
         </div>
     );
   }
